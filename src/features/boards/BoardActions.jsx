ad0834b8534a2b97e26448b389b04cbd8428b039
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-05-31 03:17:14 +0600

    formatting

--- src/features/boards/BoardActions.jsx
+++ src/features/boards/BoardActions.jsx
@@ -1,8 +1,8 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { deleteBoard } from "./boardsSlice";
-import { deleteList } from "../lists/listsSlice"
-import { deleteTask } from "../tasks/tasksSlice"
+import { deleteList } from "../lists/listsSlice";
+import { deleteTask } from "../tasks/tasksSlice";
 
 const BoardActions = ({ board, setShowBoardActions, boardEditMode, setBoardEditMode }) => {
   const dispatch = useDispatch();
@@ -11,24 +11,28 @@ const BoardActions = ({ board, setShowBoardActions, boardEditMode, setBoardEditM
   const isDisabled = boardEditMode;
 
   const boardEditHandler = () => {
-    setBoardEditMode(prev => !prev);
-    setShowBoardActions(prev => !prev);
+    setBoardEditMode((prev) => !prev);
+    setShowBoardActions((prev) => !prev);
   };
 
   const boardDeleteHandler = () => {
     navigate("/boards/", { replace: true });
 
     dispatch(deleteBoard(board.boardId));
-    board.boardLists.forEach(listId => dispatch(deleteList(listId)))
-    board.boardTasks.forEach(taskId => dispatch(deleteTask(taskId)))
+    board.boardLists.forEach((listId) => dispatch(deleteList(listId)));
+    board.boardTasks.forEach((taskId) => dispatch(deleteTask(taskId)));
 
     setShowBoardActions(false);
   };
 
   return (
     <div className="board-actions">
-      <button className="btn btn-sm btn-secondary" onClick={boardEditHandler} disabled={isDisabled}>Edit</button>
-      <button className="btn btn-sm btn-danger" onClick={boardDeleteHandler} disabled={isDisabled}>Delete</button>
+      <button className="btn btn-sm btn-secondary" onClick={boardEditHandler} disabled={isDisabled}>
+        Edit
+      </button>
+      <button className="btn btn-sm btn-danger" onClick={boardDeleteHandler} disabled={isDisabled}>
+        Delete
+      </button>
     </div>
   );
 };
