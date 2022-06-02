134f13750b1ac43c02a65a9e7e730802ae38b900
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-06-02 21:12:43 +0600

    understanding recursion further

--- PlayGround/recursion.js
+++ PlayGround/recursion.js
@@ -1,6 +1,14 @@
-function pow(base, exponent) {
-  if (exponent === 0) return 1;
-  else return base * pow(base, exponent - 1);
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
 }
 
-const result = pow(2, 4);
+console.log(findSolution(24));
