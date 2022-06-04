3bd6c409a788dbb0836e1e60ca5b2114cb3279dd
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-06-04 12:32:55 +0600

    understanding linked lists further

--- PlayGround/linkedList.js
+++ PlayGround/linkedList.js
@@ -1,49 +1,44 @@
 const arrayToList = (arr) => {
   let list = null;
+
   for (let i = arr.length - 1; i >= 0; i--) {
+    const value = arr[i];
     list = {
-      value: arr[i],
+      value,
       rest: list,
     };
   }
+
   return list;
 };
 
-console.log(arrayToList([10, 20]));
-// → {value: 10, rest: {value: 20, rest: null}}
+console.log(arrayToList([1, 2, 3]));
 
 const listToArray = (list) => {
   let arr = [];
+
   for (let node = list; node; node = node.rest) {
     arr.push(node.value);
   }
+
   return arr;
 };
 
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
+console.log(listToArray(arrayToList([1, 2, 3])));
 
-const prepend = (element, list) => {
+const prepend = (list, value) => {
   return {
-    value: element,
+    value,
     rest: list,
   };
 };
 
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-
-// const nth = (list, position) => {
-//   for (let node = list, i = 0; node; node = node.rest, i++) {
-//     if (i === position) return node.value;
-//   }
-// };
+console.log(prepend(arrayToList([1, 2]), 3));
 
 const nth = (list, n) => {
-  if (!list) return undefined;
-  else if (n === 0) return list.value;
-  else return nth(list.rest, n - 1);
+  for (let node = list, i = 0; node; node = node.rest, i++) {
+    if (i === n) return node.value;
+  }
 };
 
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
+console.log(nth(arrayToList([1, 2, 3]), 2));
