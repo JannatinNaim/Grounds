5ebd98a00814047a9bd05d25712295b0cb259e02
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-07 14:19:54 +0600

    Python - Type annotations

--- PlayGround/main.py
+++ PlayGround/main.py
@@ -1,13 +1,14 @@
-x = {
-    "a": 1,
-    "b": 2,
-    "c": 3
-}
+def sum_of_n_nums(start: int, n: int) -> int:
+    if n == start:
+        return start
+    return sum_of_n_nums(start, n - 1) + n
 
-y = {
-    "a": 1,
-    "c": 3,
-    "b": 2
-}
 
-print(x == y)
+x = sum_of_n_nums(100, 101)
+
+
+def string_to_int(num: int) -> str:
+    return str(num)
+
+
+y = string_to_int(123)
